1296c32e0005d4637072298cd5eb5474aec97198
Ghanbar <ghanbar.naruto.78@gmail.com>
2022-01-28 19:44:31 +0330

    change login form

--- client/app.js
+++ client/app.js
@@ -9,6 +9,8 @@ const socket = io('ws://localhost:3000', {
  *********************/
 const loginForm = document.getElementById('login-form');
 const masterHead = document.querySelector('.masthead');
+const loginErrorMessage = document.getElementById('login-error');
+
 
 const messages = document.getElementById('messages');
 const messageInputForm = document.getElementById('message-input');
@@ -37,13 +39,12 @@ socket.on('login:error', (data) => {
 
     // find elements
     const usernameEl = loginForm.querySelector('input[type="text"]');
-    const errorMessage = loginForm.querySelector('.error');
 
     // create message
     let message = '';
     message += '<li>username or password are wrong</li>';
     message += '<li>please enter info again</li>';
-    errorMessage.innerHTML = `<ul>${message}</ul>`;
+    loginErrorMessage.innerHTML = `<ul>${message}</ul>`;
 
     // change focus to elements
     usernameEl.focus();
@@ -76,22 +77,21 @@ loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const usernameEl = loginForm.querySelector('input[type="text"]');
     const passwordEl = loginForm.querySelector('input[type="password"]');
-    const errorMessage = loginForm.querySelector('.error');
 
     // check inputs
-    errorMessage.classList.add('display-none');
+    loginErrorMessage.classList.add('display-none');
     let message = '';
     if (!usernameEl.value) {
-        errorMessage.classList.remove('display-none');
-        message += '<li>please input username</li>';
+        loginErrorMessage.classList.remove('display-none');
+        message += '<li>please enter your username</li>';
         usernameEl.focus();
     }
     if (!passwordEl.value) {
-        errorMessage.classList.remove('display-none');
-        message += '<li>please input username</li>';
+        loginErrorMessage.classList.remove('display-none');
+        message += '<li>please enter your password</li>';
         passwordEl.focus();
     }
-    errorMessage.innerHTML = `<ul>${message}</ul>`;
+    loginErrorMessage.innerHTML = `<ul>${message}</ul>`;
 
     // send information to server
     let username = usernameEl.value;
