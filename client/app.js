98e4431542ae1086adccc155a82e64a52b96e337
Ghanbar <ghanbar.naruto.78@gmail.com>
2022-01-28 18:23:12 +0330

    add login handlers

--- client/app.js
+++ client/app.js
@@ -1,7 +1,12 @@
+// create socket
 const socket = io('ws://localhost:3000', {
     path: '/socket'
 });
 
+
+/********************
+ * Element finding
+ *********************/
 const loginForm = document.getElementById('login-form');
 const masterHead = document.querySelector('.masthead');
 
@@ -9,12 +14,64 @@ const messages = document.getElementById('messages');
 const messageInputForm = document.getElementById('message-input');
 const messageInput = messageInputForm.querySelectorAll('input[type=text]')[0];
 
-socket.on('message', (text) => {
+
+/**********************
+ *   Login Handlers
+ ***********************/
+socket.on('login:success', (data) => {
+    /* if login successfully */
+
+    // hide master head
+    masterHead.classList.add('display-none');
+
+    // show input
+    messageInputForm.classList.remove('display-none');
+    messageInput.focus();
+
+    // get chats
+    socket.emit('get-all-chats');
+});
+
+socket.on('login:error', (data) => {
+    /* if login failure */
+
+    // find elements
+    const usernameEl = loginForm.querySelector('input[type="text"]');
+    const errorMessage = loginForm.querySelector('.error');
+
+    // create message
+    let message = '';
+    message += '<li>username or password are wrong</li>';
+    message += '<li>please enter info again</li>';
+    errorMessage.innerHTML = `<ul>${message}</ul>`;
+
+    // change focus to elements
+    usernameEl.focus();
+});
+
+/**********************
+ *   User Handlers
+ ***********************/
+socket.on('get-all-chats', (data) => {
+    console.log(data);
+});
+
+socket.on('add-chat', (data) => {
+
+});
+
+socket.on('message', (data) => {
     const el = document.createElement('li');
-    el.innerHTML = text;
+    el.innerHTML = data;
     messages.appendChild(el);
 });
 
+
+/****************************
+ *   Event Listeners
+ *****************************/
+
+// login: submit
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const usernameEl = loginForm.querySelector('input[type="text"]');
@@ -26,7 +83,7 @@ loginForm.addEventListener('submit', (e) => {
     let message = '';
     if (!usernameEl.value) {
         errorMessage.classList.remove('display-none');
-        message = '<li>please input username</li>';
+        message += '<li>please input username</li>';
         usernameEl.focus();
     }
     if (!passwordEl.value) {
@@ -40,15 +97,11 @@ loginForm.addEventListener('submit', (e) => {
     let username = usernameEl.value;
     let password = passwordEl.value;
     console.log(JSON.stringify({username, password}));
-    data = JSON.stringify({username, password});
+    const data = JSON.stringify({username, password});
     socket.emit('login', data);
-
-    // if login successfully
-    masterHead.classList.add('display-none');
-    messageInputForm.classList.remove('display-none');
-    messageInput.focus();
 });
 
+// message: submit
 messageInputForm.addEventListener('submit', (e) => {
     e.preventDefault();
     if (messageInput.value) {
